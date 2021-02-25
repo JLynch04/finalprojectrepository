9a56c1b74d894a28009dcff76ad6fb6f1a460de2
JLynch04 <71776551+JLynch04@users.noreply.github.com>
2021-02-25 13:46:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 const searchClient = algoliasearch('YMQ9NRXP2Y', '293d2e6fb11b087ec1ae1bafa5077476');
+const search = instantsearch({
+indexName: 'Email-Index',
+searchClient,
+});
+
+search.addWidgets([
+instantsearch.widgets.searchBox({
+container: '#searchbox',
+}),
+
+instantsearch.widgets.configure({
+hitsPerPage: 16
+}),
+
+instantsearch.widgets.hits({
+container: '#hits',
+templates: {
+item: `
+<div>
+<div class="hit-name">
+{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+</div>
+</div>
+`,
+},
+}),
+
+instantsearch.widgets.pagination({
+container: '#pagination',
+}),
+]);
+
+search.start();
